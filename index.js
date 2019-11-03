c588eaa7bfadf64d66a2348c8ee6b16ba10a6657
Alyona <itsbronza@yahoo.com>
2019-11-03 17:40:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function appendKitten(name){
 return [...kittens, name];
 }
 
-function prependKitten(name) {
+function prependKitten(name){
 return[name, ...kittens];
+}
+
+function removeLastKitten(){
+  
 }
